6b1eb2cd727c253eae3ca3e33669f7f04107f5a3
jccetinac <editorialdartagnan@iMac-de-EDITORIAL.local>
2021-03-23 18:43:48 -0500

    add environment variables

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 
 // importa express el router y bodyParser
 const express = require('express');
+require('dotenv').config();
+console.log(process.env);
+
 const bodyParser = require('body-parser');
 const router = require('./network/routes');
 const cors = require('cors');
 
-
-//const response = require('./network/response');
-
-
-// inicia la aplicacion express  y le añade router y parser
 var app = express();
 app.use(bodyParser.json());
 
 router(app);
 
-//servir estaticos
 app.use('/app', express.static('public'));
 app.use(cors());
 
-
-
-app.listen(4200);
-
-console.log('la app escucha en http://localhost:4200');
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+});
