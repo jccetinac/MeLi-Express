7f44e09430e8bdc87c5aac0e6d65ba03b684f2de
jccetinac <editorialdartagnan@iMac-de-EDITORIAL.local>
2021-03-24 16:37:38 -0500

    add statics files

---
+++ public/precache-manifest.e791c201d8e6c40522c5e65abdf21d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a080666df35c83852885a3feafa8cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81e8bb7611320390294d",
+    "url": "/static/css/main.75299e43.chunk.css"
+  },
+  {
+    "revision": "1dd5fd1d36b4a11915cd",
+    "url": "/static/js/2.8e266f9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e266f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e8bb7611320390294d",
+    "url": "/static/js/main.373ebb15.chunk.js"
+  },
+  {
+    "revision": "e08f888d9a08f8c84965",
+    "url": "/static/js/runtime-main.f2bb1637.js"
+  }
+]);
