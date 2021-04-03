0f70b262f4923ff0aaf49852bbc192a98dddb909
jccetinac <editorialdartagnan@iMac-de-EDITORIAL.local>
2021-04-02 20:37:26 -0500

    enable cors

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ const cors = require('cors');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 router(app);
 
 app.use('/app', express.static('public'));
-app.use(cors());
+
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
